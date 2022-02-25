387094113aa75b4f850e9d1acfba843d7d0e46af
aryanfanid <aryan.fanid90@yahoo.com>
2022-02-25 16:57:54 +0330

    PySimpleGUI Integration

--- downloader.py
+++ downloader.py
@@ -1,26 +1,22 @@
-#=====================================================
-# Command Line Version.(Linux Only)
-#=====================================================
-# import os
-#
-# downloadList = open("list.txt", "r+").readlines()
-# for link in downloadList:
-#     try:
-#         print("Downloading {}...".format(link))
-#         os.system("youtube-dl -f bestaudio --extract-audio --audio-format mp3 \
-#     --audio-quality 0 {}".format(link))
-#     except Error as e:
-#         with open("failed.txt", "w+") as failed:
-#             failed.writeline(link, "\t", e)
-#=====================================================
-#
-#
-#=====================================================
-# Python Library Version.(Cross Platform)
-#=====================================================
-
 from __future__ import unicode_literals
 import youtube_dl
+import PySimpleGUI as sg
+from random import randrange
+
+DOWNLOAD_DIR = '/home/aryanfanid/Desktop'
+
+# Add a touch of color
+theme_name_list = sg.theme_list()
+sg.theme(theme_name_list[randrange(len(theme_name_list))])
+
+# All the stuff inside your window.
+layout = [[sg.Text('YouTube URL:'), sg.InputText()],
+          [sg.Text('Download Directory:'), sg.InputText(
+              default_text=DOWNLOAD_DIR)],
+          [sg.Button('Download'), sg.Button('Cancel'), sg.Text('', key='-ds-')]]
+
+# Create the Window
+window = sg.Window('YouTube Downloader', layout)
 
 
 class MyLogger(object):
@@ -50,8 +46,12 @@ ydl_opts = {
     'progress_hooks': [my_hook],
 }
 
-links = open("linksList.txt", "r+").readlines()
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([values[0]])
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    for link in links:
-        ydl.download([link])
+window.close()
